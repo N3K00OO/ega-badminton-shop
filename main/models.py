6b8579e14256bea85d6278ed5885770bc82cb53a
Ega Sudjali <egasudjali1989@gmail.com>
2025-09-07 03:06:02 +0700

    done mostly

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
+class Product(models.Model):
+    name =  models.CharField(max_length=100)
+    price =  models.IntegerField()
+    description =  models.TextField()
+    thumbnails  = models.URLField()
+    category =  models.CharField(max_length=100)
+    is_featurted = models.BooleanField(default=False)
+
+def __str__(self):
+    return f"{self.name}({self.category})"
+
+
 # Create your models here.
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    ctx = {
+        "app_name": "main",
+        "student_name": "Gregorius Ega Aditama Sudjali",
+        "class_name": "Kelas kamu",
+    }
+    return render(request, "main/home.html", ctx)
