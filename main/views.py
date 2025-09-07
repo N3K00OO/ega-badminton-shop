ccbfe7a35dd25422a020ca1fc7ce94bd8d7292d9
Ega Sudjali <egasudjali1989@gmail.com>
2025-09-07 18:01:09 +0700

    fix name

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def home(request):
     ctx = {
         "my_app_name": "Toko Bola Ega",
-        "student_name": "Gregorius Ega Aditama Sudjali",
+        "my_name": "Gregorius Ega Aditama Sudjali",
         "pbp_class": "PBP c",
     }
     return render(request, "main/home.html", ctx)
