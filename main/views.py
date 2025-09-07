fdc1282fc86b3f76e94c8bcb2d5cff8e651f0532
Ega Sudjali <egasudjali1989@gmail.com>
2025-09-07 18:02:05 +0700

    fix class to capital C

--- main/views.py
+++ main/views.py
@@ -4,6 +4,6 @@ def home(request):
     ctx = {
         "my_app_name": "Toko Bola Ega",
         "my_name": "Gregorius Ega Aditama Sudjali",
-        "pbp_class": "PBP c",
+        "pbp_class": "PBP C",
     }
     return render(request, "main/home.html", ctx)
